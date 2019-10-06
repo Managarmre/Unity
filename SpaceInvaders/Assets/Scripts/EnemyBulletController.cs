2b20b009900d4ea38b41740918edada1b9c2bf85
Managarmr <paprika.dono@gmail.com>
2019-10-06 21:48:20 +0200

    Space Invaders game

---
+++ SpaceInvaders/Assets/Scripts/EnemyBulletController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBulletController : MonoBehaviour
+{
+	private Transform bullet;
+	public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bullet = GetComponent<Transform>();
+    }
+
+    void FixedUpdate()
+    {
+    	bullet.position += Vector3.up * -speed;
+
+    	// outside of the screen
+    	if (bullet.position.y <= -10)
+    		Destroy(bullet.gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+    	// hit player
+        if (other.tag == "Player")
+        {
+        	Destroy(other.gameObject);
+        	Destroy(gameObject);
+        	GameOver.isPlayerDead = true;
+        }
+        // if a bullet hit a base, reduce health of base
+        else if (other.tag == "Base")
+        {
+        	GameObject playerBase = other.gameObject;
+        	BaseHealth baseHealth = playerBase.GetComponent<BaseHealth>();
+        	baseHealth.health -= 1;
+        	Destroy(gameObject);
+        }
+    }
+}
---
+++ SpaceInvaders/Assets/Scripts/RestartLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartLevel : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+    	// restart the level if the player push 'R'
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+        	// initialize score and player
+        	PlayerScore.playerScore = 0;
+        	GameOver.isPlayerDead = false;
+        	Time.timeScale = 1;
+
+        	// load scene named Scene_001
+        	SceneManager.LoadScene("Scene_001");
+        }
+    }
+}
